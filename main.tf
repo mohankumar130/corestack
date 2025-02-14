dfacf80f1cc56cb76d233bde045def86a8fc7040
mohan <mohankumar@ck.com>
2025-02-14 07:38:01 +0530

    added

--- main.tf
+++ main.tf
@@ -3,12 +3,11 @@ data "aws_instances" "instances_with_tag" {
     name   = "tag:${var.filter_tag_key}"
     values = ["${var.filter_tag_value}"]
   }
-
   instance_state_names = ["running", "stopped"]
 }
 
 data "aws_instance" "filtered_instances" {
-  for_each   = toset(data.aws_instances.instances_with_tag.ids)
+  for_each    = toset(data.aws_instances.instances_with_tag.ids)
   instance_id = each.key
 }
 
@@ -16,27 +15,37 @@ locals {
   cutoff_date_local = timeadd(timestamp(), "${-(var.cutoff_days * 86400)}s")
 }
 
-# Fetch EBS Volume Details
-data "aws_ebs_volume" "volumes" {
-  for_each  = toset(flatten([
+# Fetch all EBS Volumes
+data "aws_ebs_volumes" "all_volumes" {}
+
+# Create a map of volume IDs to their names
+locals {
+  volume_name_map = { 
+    for vol in data.aws_ebs_volumes.all_volumes.ids : 
+    vol => lookup(data.aws_ebs_volume.volume_details[vol].tags, "Name", vol) 
+  }
+}
+
+data "aws_ebs_volume" "volume_details" {
+  for_each  = toset(data.aws_ebs_volumes.all_volumes.ids)
+  volume_id = each.key
+}
+
+resource "aws_ebs_snapshot" "snapshots" {
+  for_each = toset(flatten([
     for instance in data.aws_instance.filtered_instances :
     concat(
       [for root in instance.root_block_device : root.volume_id],
       [for ebs in instance.ebs_block_device : ebs.volume_id]
     )
   ]))
-  volume_id = each.key
-}
 
-resource "aws_ebs_snapshot" "snapshots" {
-  for_each = data.aws_ebs_volume.volumes
-  
-  volume_id   = each.value.id
-  description = "Snapshot of ${lookup(each.value.tags, "Name", each.value.id)}"
+  volume_id   = each.value
+  description = "Snapshot of ${lookup(local.volume_name_map, each.value, each.value)}"
 
   tags = {
     "${var.filter_tag_key}" = var.filter_tag_value
-    "Name"                  = "Snapshot-${lookup(each.value.tags, "Name", each.value.id)}"
+    "Name"                  = "Snapshot-${lookup(local.volume_name_map, each.value, each.value)}"
   }
 
   timeouts {
@@ -64,7 +73,10 @@ output "instance_ids" {
 }
 
 output "volume_ids" {
-  value = keys(data.aws_ebs_volume.volumes)
+  value = flatten([
+    for inst in data.aws_instance.filtered_instances : 
+    [for bd in inst.ebs_block_device : bd.volume_id]
+  ])
 }
 
 output "snapshot_ids" {
